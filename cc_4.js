e9ffe11eef568152dfad3488fd1c1119eb1308b6
Michael Rueda <michael.a.rueda22@gmail.com>
2024-05-24 11:57:59 -0400

    Create EV class extending Car with additional charge property

---
+++ cc_4.js
@@ -0,0 +1,24 @@
+//U22562170
+
+class Car {
+    constructor(make,speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+accelerate () {
+    this.speed += 10;
+    }
+
+brake () {
+    this.speed -= 5;
+    }
+
+}
+
+class EV extends Car {
+    constructor(make, speed, charge) {
+        super(make, speed);
+        this.charge = charge;
+    }
+}
